f59fe313930d4a22936cb718b40f2a68b9db6bc2
Miguel Ángel de la Rosa Leva <miguelangeldelarosaleva@gmail.com>
2025-02-17 18:08:18 +0100

    update estilos bootstrap

--- agriario/src/main/java/com/example/agriario/AgriarioApplication.java
+++ agriario/src/main/java/com/example/agriario/AgriarioApplication.java
@@ -2,14 +2,14 @@ package com.example.agriario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class AgriarioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AgriarioApplication.class, args);
 
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		System.out.println(encoder.encode("user123"));
+		// BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		// System.out.println(encoder.encode("user123"));
 	}
 }
